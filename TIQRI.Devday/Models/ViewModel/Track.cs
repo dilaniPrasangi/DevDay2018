1a672b0b654ad8dbfa83083c3c6b1040b9ec2889
dilani15farm <dilani15farm@DEV3SPAPP>
2018-06-19 18:04:17 +1000

    track updates

--- TIQRI.Devday/Models/ViewModel/Track.cs
+++ TIQRI.Devday/Models/ViewModel/Track.cs
@@ -7,7 +7,7 @@ namespace TIQRI.Devday.Models
 {
     public class Track:EntityBase
     {
-        public string Name { get; set; }
+        public string TrackName { get; set; }
         public string Description { get; set; }
         public Event Event { get; set; }
     }
