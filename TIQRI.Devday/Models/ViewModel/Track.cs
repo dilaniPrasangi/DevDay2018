a516dd38dd06f6af9719c7c449144ce9a641757f
Bashitha Wijegunawardana <lbw@tiqri.com>
2018-06-20 15:04:10 +0530

    changed the TrackName field to Name field of Track table

--- TIQRI.Devday/Models/ViewModel/Track.cs
+++ TIQRI.Devday/Models/ViewModel/Track.cs
@@ -7,7 +7,7 @@ namespace TIQRI.Devday.Models.ViewModel
 {
     public class Track:EntityBase
     {
-        public string TrackName { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public Event Event { get; set; }
     }
